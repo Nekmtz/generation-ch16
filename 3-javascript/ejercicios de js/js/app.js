a90b62d33bf64d05e8d48fe108d9af32e32762d1
Carlos Marínez <carlos.hernandezm@politicas.unam.mx>
2022-08-04 17:22:07 -0500

    arreglos y formacion de equipos para el proyecto

--- 3-javascript/ejercicios de js/js/app.js
+++ 3-javascript/ejercicios de js/js/app.js
@@ -43,37 +43,23 @@
 
 
 //// 4 /
+// let num=Math.floor(Math.random()*100);
 
-let numeroRandom = Math.random()
-let a = prompt ("adivina el numero, cuando quieras dejar de jugar escribe -bye-");
-
-for ()
-// if (numeroRandom === a){
-// console.log("felcidades, le atinaste es " + numeroRandom );
-// prompt ("felcidades, le atinaste es " + numeroRandom);
-// } else if (numeroRandom <= a ){
-
-//     console.log("el numero magico es menor");
-//     prompt ("intante de nuevo el numero magico es menor")
-
-// } else if (numeroRandom >= a){
-//     console.log("el numero es mayor")
-//     prompt ("intante de nuevo el numero es mayor ")
-// } else if (numeroRandom = "bye") {
-//     prompt ("adios burro")
+// let adivina=1*prompt("¿Qué número crees que es? ");
+// let salir=0;
+// while (adivina!==num && salir!==1){
+//     if (adivina>num){
+//         adivina=1*prompt("el núm ingresado "+adivina+" es muy grande \n¿Qué número crees que es? ");
+//     }else if(adivina<num){
+//         adivina=1*prompt("el núm ingresado "+adivina+" es muy peque \n¿Qué número crees que es? ");
+//     }else{
+//         salir=1*prompt("Ingresa 1 para salir");
+//     }
 // }
 
-
-}
-  
-
-
-
-
-
-
-
-
+// if (adivina==num){
+//     alert("Acertaste");
+// }
 
 
 
@@ -81,14 +67,31 @@ for ()
 
 // cancer, calculadora 
 
-// virgo alertas y calculadora
+let gradosCentigrados = Number(prompt ("ingrese los grados"))
+console.log(typeof(gradosCentigrados));
+let unidadGrado = prompt ("ingreese la unidad de los grados y la converción (c-f, c-k, k-f...) ")
+
+console.log(typeof(grados));
+
+switch (unidadGrado){
+case "c-f":
+
+alert ("son " + (gradosCentigrados * 1.8 + 32) + " grados farenheit" )
+break
+case "c-k":
+alert ("son " + (gradosCentigrados + 275.15) + " grados kelvin" )
+break
+
+case "k-c":
+    let kace = function (gradosCentigrados, b ) {
+        return (gradosCentigrados - b);
+    };
+    let resultado1 = kace (gradosCentigrados, 275);
+    alert (resultado1);
+    break
+}
 
 
-/// escorpio con un swicth o if else
 
 
-/// picis con un for 
-
-
-
-/// 
+// function convertir 
