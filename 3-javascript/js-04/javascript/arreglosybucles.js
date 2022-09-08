057edb4c891c6d5250eed65d2a0555fc4d7118ab
Carlos Marínez <carlos.hernandezm@politicas.unam.mx>
2022-09-07 19:11:38 -0500

    nueva carpeta

--- 3-javascript/js-04/javascript/arreglosybucles.js
+++
@@ -1,9 +0,0 @@
-/// for concatenado
-
-for (let i = 0; i <5; i++){
-
-console.log("for principal " + i);
-for (let j = 0; j <i;j++){
-console.log("for interno" + j);
-}
-}
--- java/java-01/repo-jdk/Hola1.java
+++
@@ -1,6 +0,0 @@
-class Hola {
-public static void main (String arg []){
-
-System.out.prinln("hola dos");
-}
-}
